a8602801e9a6b7d642e111a5b0c2f22f1e0bc8e2
Filippo Cerpelloni <filippo.cerpelloni@gmail.com>
2024-03-18 22:01:10 +0100

    started correlation with language properties

--- code/stats/stats_stimuli_properties.py
+++ code/stats/stats_stimuli_properties.py
@@ -44,18 +44,66 @@ def stats_further_analyses(opt):
     # - re-organize results summary to have something based on words and not on sessions
     
     # Load stimuli results 
+    leResults = pd.read_csv(os.path.join(opt['dir']['stats'], 'datasets', 
+                                         'VBT_stimuli-letters_desc-behavioural-results.csv'))
     trResults = pd.read_csv(os.path.join(opt['dir']['stats'], 'datasets', 
-                                  'VBT_stimuli-training_desc-behavioural-results.csv'))
+                                         'VBT_stimuli-training_desc-behavioural-results.csv'))
     teResults = pd.read_csv(os.path.join(opt['dir']['stats'], 'datasets', 
-                                  'VBT_stimuli-test_desc-behavioural-results.csv'))
+                                         'VBT_stimuli-test_desc-behavioural-results.csv'))
     
     # Load stimuli statistics
+    trStats = pd.read_csv(os.path.join(opt['dir']['stats'], 'datasets', 
+                                       'VBT_stimuli-training_desc-list-with-stats.csv'))
+    teStats = pd.read_csv(os.path.join(opt['dir']['stats'], 'datasets', 
+                                       'VBT_stimuli-test_desc-list-with-stats.csv'))
     
-    # Compute letter-wise averages across repetitions
+    ## Letters
+    # Averages across repetitions and across subjects
+    leRepAverages = leResults.groupby(['subject', 'script', 'letter']).agg({
+        'readingTime': 'mean', 
+        'checkingTime': 'mean'})
+    leAverages = leRepAverages.groupby(['script', 'letter']).agg({
+        'readingTime': 'mean', 
+        'checkingTime': 'mean'})
     
-    # Compute word-wise distance averages for test
+    ## Training
+    # Separate tested and non-tested items, to compute different averages 
+    trTested = trResults[trResults['tested'] == 1]
+    trTested = trTested[['subject', 'script', 'session', 'woord', 'response', 
+                         'score', 'writingTime', 'checkingTime']]
+    
+    trTestedAverages = trTested.groupby(['script', 'session', 'woord']).agg({
+        'score': 'mean',
+        'writingTime': 'mean',
+        'checkingTime': 'mean'
+        }).reset_index()
+    
+    trNotTested = trResults[trResults['tested'] == 0]
+    trNotTested = trNotTested[['subject', 'script', 'session', 'woord', 'readingTime', 'checkingTime']]
+    
+    trNotTestedAverages = trNotTested.groupby(['script', 'session', 'woord']).agg({
+        'readingTime': 'mean',
+        'checkingTime': 'mean'
+        }).reset_index()
+    
+    ## Test
+    # Average scores, writing time, distance from the correct answer
+    teAverages = teResults.groupby(['script', 'session', 'woord']).agg({
+        'score': 'mean',
+        'writingTime': 'mean',
+        'distance': 'mean'
+        }).reset_index()
+    
+    # Separate type of stimulus (no stats for pseudo-words) and groups (BR v. CB)
+    
     
     # Correlate averages with language statistics
+    # Test: score, writing time, distance (negative) with length, number syllables, frequency
+    teCorrStats = teStats[['length', 'syllabels', 'frequency', 'phonemes', 'old20', 'pld30']]  
+    teCorrAverages = teAverages[['score', 'writingTime', 'distance']]    
+    teCorrelations = teCorrStats.corrwith(teCorrAverages)
+
+    
     
     # Save results
     
